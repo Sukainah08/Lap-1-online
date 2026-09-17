cf61b8cb8a621bc9a5dd9bc8925224c4616b67b1
Sukainah Ali A Al_Obaid <2270002359@iau.edu.sa>
2026-09-17 07:42:33 +0300

    Add files via upload
    Task2 c++

---
+++ Task1.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int v1,v2,product;
+    cout<<"Enter the first number";
+    cin>>v1;
+    cout<<"Enter the sewcond number";
+    cin>>v2;
+    product=v1*v2;
+    cout<<"The product is:"<<product;
+    return 0;
+}
